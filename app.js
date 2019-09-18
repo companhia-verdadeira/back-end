19121cc9c43b4196b09038f0eb1983af3a6c270b
alexfogaca96 <alex.fogaca.96@gmail.com>
2019-09-18 04:52:41 -0300

    Add CORS and Content-Type.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const env = require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/', (req, res) => {
+    res.header("Content-Type", "text/plain");
+    res.send('Hello, world!')
+});
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}`));
